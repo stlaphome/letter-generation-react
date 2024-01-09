ce639393be33960bad5bca9519712e497644da90
Vikas Ayinampudi <vikas.s@ivtlinfoview.co.jp>
2024-01-09 14:38:34 +0530

    compile issue fix

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import "./App.css";
 
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { useEffect, useState } from "react";
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from "@mui/material";
-
-import TextEditor from "./components/DynamicReport/TextEditor";
-import TriggerView from "./components/DynamicReport/TriggerView";
 import Pagelayout from "./components/Dashboard/Pagelayout";
-import PageNotFound from "./components/Dashboard/PageNotFound";
+import PageNotFound from "./components/CustomComponents/PageNotFound";
 
 function App() {
   return (
     <>
       <div className="App">
         <Routes>
-        <Route
-              path={`/lettergeneration/home/*`}
-              element={<Pagelayout />}
-            />
+          <Route path={`/lettergeneration/home/*`} element={<Pagelayout />} />
           <Route path="*" element={<PageNotFound />} />
-                 </Routes>
+        </Routes>
       </div>
     </>
   );
